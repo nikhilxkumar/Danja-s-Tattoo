99f2fb455c7c9b033fa1ce3c0c2a2c5d8c003c09
nikhilxkumar <nikhilxkumar03@gmail.com>
2024-06-12 17:55:33 +0200

    update info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,58 +4,17 @@ import Button from "../layouts/Button";
 const Contact = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center md:mx-32 mx-5 mt-10">
-      <div className="flex flex-col md:flex-row justify-between w-full">
-      <h1 className="text-5xl font-semibold text-center md:text-start mt-14 md:mt-0">Contact</h1>
-      <p>+49-173-3951881</p>
-        {/* <form className="w-full md:w-2/5 space-y-5 pt-20">
-          <h1 className="text-5xl font-semibold text-center">Contact Us</h1>
-          <div className="flex flex-col">
-            <label htmlFor="userName">Your Name</label>
-            <input
-              className="py-3 px-5 rounded-lg hover:shadow-md transition-all bg-gray-100 text-gray-800 focus:outline-none focus:ring-2 focus:ring-brightRed"
-              type="text"
-              name="userName"
-              id="userName"
-              placeholder="Enter your Name"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="userEmail">Your Email</label>
-            <input
-              className="py-3 px-5 rounded-lg hover:shadow-md transition-all bg-gray-100 text-gray-800 focus:outline-none focus:ring-2 focus:ring-brightRed"
-              type="email"
-              name="userEmail"
-              id="userEmail"
-              placeholder="Enter your Email"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="userNumber">Your Number</label>
-            <input
-              className="py-3 px-5 rounded-lg hover:shadow-md transition-all bg-gray-100 text-gray-800 focus:outline-none focus:ring-2 focus:ring-brightRed"
-              type="number"
-              name="userNumber"
-              id="userNumber"
-              placeholder="Enter your Number"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="userMessage">Your Message</label>
-            <textarea
-              className="py-3 px-5 rounded-lg hover:shadow-md transition-all bg-gray-100 text-gray-800 focus:outline-none focus:ring-2 focus:ring-brightRed"
-              name="userMessage"
-              id="userMessage"
-              rows="4"
-              placeholder="Enter your Message"
-            ></textarea>
-          </div>
-          <div className="flex flex-row justify-center space-x-4">
-            <Button title="Send Message" />
-          </div>
-        </form> */}
-        
+        <h1 className="text-5xl font-semibold text-center md:text-start mt-14 md:mt-0">
+        Contact
+        </h1>
+
+        <div className=" w-full md:w-3/4 space-y-5 mt-4 text-2xl ">
+          <p>
+          +49-173-3951881
+            </p>
+          <p><a href="https://www.instagram.com/danjas_tattoo_/">My Instagram</a> (Preferred)</p>
+        </div>
       </div>
-    </div>
   );
 };
 
