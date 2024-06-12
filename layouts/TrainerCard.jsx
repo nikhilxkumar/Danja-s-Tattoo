b7a1196c08ab4229d8406e3434403eedeca5f586
nikhilxkumar <nikhilxkumar03@gmail.com>
2024-06-12 17:15:11 +0200

    added about section

--- layouts/TrainerCard.jsx
+++ layouts/TrainerCard.jsx
@@ -11,6 +11,41 @@ const TrainerCard = (props) => {
           alt="Trainer"
         />
       </div>
+      <div className="text-center">
+        <h1 className="text-xl font-semibold py-2">{props.name}</h1>
+        <div className="flex justify-center gap-4">
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsInstagram
+              size={25}
+              className="hover:text-[#e53961] cursor-pointer"
+            />
+          </a>
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsFacebook
+              size={25}
+              className="hover:text-[#4267B2] cursor-pointer"
+            />
+          </a>
+          <a
+            href="https://twitter.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsTwitterX
+              size={25}
+              className="hover:text-[#3f4549] cursor-pointer"
+            />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
