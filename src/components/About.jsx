f18127f5b86a6482a5aa8c9eb1d929be4bfde747
nikhilxkumar <nikhilxkumar03@gmail.com>
2024-06-12 17:01:59 +0200

    added about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className=" min-h-screen flex flex-col md:flex-row items-center md:mx-32 mx-5">
       <div>
         <h1 className="text-5xl font-semibold text-center md:text-start mt-14 md:mt-0">
-          About Us
+          About Me
         </h1>
 
         <div className=" w-full md:w-3/4 space-y-5 mt-4 text-2xl ">
